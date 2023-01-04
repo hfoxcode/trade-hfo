f4c438ea29984eda8f5bb1433c2a67436240ddce
berkayhfo <berkay.hfo@hotmail.com>
2023-01-04 16:17:54 +0300

    denem

---
+++ build/precache-manifest.41fa51a1ae490b33fc6d3b4fbe1a38be.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a51fbe07041d45a82c5",
+    "url": "/trade-hfo/static/css/main.d6eb3160.chunk.css"
+  },
+  {
+    "revision": "1a51fbe07041d45a82c5",
+    "url": "/trade-hfo/static/js/main.1a51fbe0.chunk.js"
+  },
+  {
+    "revision": "d48f06b542086f619008",
+    "url": "/trade-hfo/static/js/1.d48f06b5.chunk.js"
+  },
+  {
+    "revision": "b1cb14a862452497393c",
+    "url": "/trade-hfo/static/js/2.b1cb14a8.chunk.js"
+  },
+  {
+    "revision": "c585ec44ea24f1ad1072",
+    "url": "/trade-hfo/static/js/runtime~main.c585ec44.js"
+  },
+  {
+    "revision": "d2bbdd6135b2e58a6d7421a46a5525e1",
+    "url": "/trade-hfo/static/media/blog-text-image.d2bbdd61.png"
+  },
+  {
+    "revision": "01ba5045fdcb3b946317e26ba407546a",
+    "url": "/trade-hfo/static/media/ribu-bigger-logo.01ba5045.png"
+  },
+  {
+    "revision": "6a5381dbba605c4b0d890d0f170769d3",
+    "url": "/trade-hfo/static/media/jesse_livermore.6a5381db.png"
+  },
+  {
+    "revision": "4bfead2c4afd8ee4f0e7ee8e48e39d34",
+    "url": "/trade-hfo/static/media/pnl_chart.4bfead2c.png"
+  },
+  {
+    "revision": "0e46bf3e00a6b5397200b166aa131785",
+    "url": "/trade-hfo/static/media/discord.0e46bf3e.png"
+  },
+  {
+    "revision": "9c9a5fd878c98a0bb4ed2e75c953aa16",
+    "url": "/trade-hfo/static/media/illustration1.9c9a5fd8.png"
+  },
+  {
+    "revision": "938aeb13111d763629304a2895dff193",
+    "url": "/trade-hfo/static/media/default_avatar.938aeb13.svg"
+  },
+  {
+    "revision": "b97dd5022103daa37ed59d1d31e998ca",
+    "url": "/trade-hfo/static/media/MetaMask-Logo.b97dd502.png"
+  },
+  {
+    "revision": "28b5fae9f76e71dae5593bf1a439feab",
+    "url": "/trade-hfo/static/media/testimonials.28b5fae9.png"
+  },
+  {
+    "revision": "ff01c77ca6aa2173bab1d4b138dde9fd",
+    "url": "/trade-hfo/static/media/payout-system.ff01c77c.png"
+  },
+  {
+    "revision": "b0ad7f846ceb746afd08d1bf1e89078f",
+    "url": "/trade-hfo/static/media/tff-balance-chart.b0ad7f84.png"
+  },
+  {
+    "revision": "3b0d7a02024c835dcd01d1fc92712ad2",
+    "url": "/trade-hfo/static/media/tff-daily-returns-chart.3b0d7a02.png"
+  },
+  {
+    "revision": "703a87b9679271ee3acd79b93a3475d9",
+    "url": "/trade-hfo/static/media/tff-wins-n-losses-charts.703a87b9.png"
+  },
+  {
+    "revision": "55424805ed7c562952a665dd7daad8f3",
+    "url": "/trade-hfo/static/media/tff-profits-by-holding-times-charts.55424805.png"
+  },
+  {
+    "revision": "bfe2b5385bc47e676644444eb863e320",
+    "url": "/trade-hfo/static/media/tff-buy-sell-charts.bfe2b538.png"
+  },
+  {
+    "revision": "11e5b8c629351b63a5c35fd4b04fb01a",
+    "url": "/trade-hfo/static/media/tff-direction-allocation-charts.11e5b8c6.png"
+  },
+  {
+    "revision": "95f5c6afed65bc8da384f407f0ee30b5",
+    "url": "/trade-hfo/static/media/tff-symbols-charts.95f5c6af.png"
+  },
+  {
+    "revision": "57152a3956a3d00b25f1511102b498b0",
+    "url": "/trade-hfo/static/media/tff-symbols-allocations-charts.57152a39.png"
+  },
+  {
+    "revision": "4dbd2e442b2ac271cccecbd4917bd379",
+    "url": "/trade-hfo/static/media/trading-image-post.4dbd2e44.jpg"
+  },
+  {
+    "revision": "aa1b1c655bc509fcc92f1bc2466daaff",
+    "url": "/trade-hfo/static/media/tff-payout-certificates-image-container-area.aa1b1c65.png"
+  },
+  {
+    "revision": "7717662af5eb48412bcfa722535996e3",
+    "url": "/trade-hfo/static/media/how-to-trade-101.7717662a.jpg"
+  },
+  {
+    "revision": "e4b800a8d39708abfb54cba7dcfc9317",
+    "url": "/trade-hfo/static/media/DecorativeLine.e4b800a8.png"
+  },
+  {
+    "revision": "e051edb6be7fd1001612a67f451da612",
+    "url": "/trade-hfo/static/media/illustration2.e051edb6.png"
+  },
+  {
+    "revision": "46dad770d443bffa2c9d4272fea50652",
+    "url": "/trade-hfo/static/media/pys.46dad770.png"
+  },
+  {
+    "revision": "d216da2a83fccc11cbe587b1fb5cd9ea",
+    "url": "/trade-hfo/static/media/how_to_work.d216da2a.png"
+  },
+  {
+    "revision": "f865feafba976154363fbcff6ce1a29a",
+    "url": "/trade-hfo/static/media/Pnl_day_chart_2.f865feaf.png"
+  },
+  {
+    "revision": "7dbeb29f54b807b5eaa1783c08cb1036",
+    "url": "/trade-hfo/static/media/confetti.7dbeb29f.png"
+  },
+  {
+    "revision": "9ada16939673860eb0f5e5da20e728a9",
+    "url": "/trade-hfo/static/media/payment-credit-card-icon.9ada1693.png"
+  },
+  {
+    "revision": "c9b475d2c0e918cc820368ca7a14e70e",
+    "url": "/trade-hfo/static/media/stat-image.c9b475d2.png"
+  },
+  {
+    "revision": "78b53876db5535dfdc66cc69a27c1c08",
+    "url": "/trade-hfo/index.html"
+  }
+];
